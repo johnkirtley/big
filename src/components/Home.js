582565ae9e305b7adc4d76f0db204ae9ff044b25
johnkirtley <kirtleyj16@gmail.com>
2020-01-27 20:04:43 -0800

    Added header image

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,28 @@ import React from 'react'
 import Styled from "styled-components";
 
 
+
+const StyledHeader = Styled.div`
+background-image: url(https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80);
+background-size: cover;
+background-repeat: no repeat;
+height: 60vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const Title = Styled.h1`
+margin: 0;
+
+`
+
+
 const Home = () => {
     return (
-        <div>
-            <h1>BIG Investment Group</h1>
-        </div>
+        <StyledHeader>
+            <Title>BIG Investment Group</Title>
+        </StyledHeader>
     )
 }
 
